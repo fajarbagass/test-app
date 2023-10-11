6754d3a0bd677df977f7a452d1b0935f3e337e58
fajarbagass <fajarbagassetiawan17@gmail.com>
2023-10-11 17:36:53 +0700

    styling css

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,16 @@
+import React from "react";
+import style from "./style.module.css";
+
 function Homepage() {
   return (
     <div>
-      <h1>ini tampilan HOME</h1>
+      <h1>Ini tampilan HOME</h1>
+      <h3 className="font-home">Styling css dari App.css</h3>
+      <h3 className={style.fontHome}>Styling css dari style.module.css</h3>
+      <h3 style={{ color: "purple" }}>Styling css inline</h3>
       <a href="/produk">buka halaman produk</a>
     </div>
   );
 }
+
 export default Homepage;
